27d5e22ce30fe82e3d029923ba320429f43fb431
haranrk <haranrajkumar97@gmail.com>
2018-06-22 16:59:23 +0900

    Made local version of runner script that runs in a batch

---
+++ gene-drug-cancer-analysis/local-runs/local-runner-script.py
@@ -0,0 +1,22 @@
+import subprocess as sp
+import os 
+from pathlib import Path as pth
+main_dir = pth(os.getcwd()).resolve()
+script_dir = pth(__file__).parent.absolute()
+os.chdir(script_dir)
+
+data_sets = [
+            #  "Avana", 
+            # "GeCKO", 
+            # "RNAi_Ach", 
+            # "RNAi_merged", 
+            "RNAi_Nov_DEM"
+            ]
+iter = 300
+trials = 100
+k_list = list(range(3,20))
+
+for data in data_sets:
+    print("Starting batch run for: %s"%data)
+    for k in k_list:
+        a=sp.run(args=['python3','../run-nmf.py','-v', data,str(k), str(iter), str(trials)])
